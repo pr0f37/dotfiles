4366fc5d66f957dc40281e4f345745079ba7aeb4
pr0f37 🚀 <adam.nowik@gmail.com>
2025-12-14 08:23:14 +0100

    wezterm and catpuccin colortheme change

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,6 +4,7 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+local act = wezterm.action
 -- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
@@ -12,11 +13,29 @@ config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
 config.font_size = 14
-config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font_with_fallback({
-	"JetBrainsMono Nerd Font Mono",
-})
+config.color_scheme = "Catppuccin Frappe"
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.hide_tab_bar_if_only_one_tab = true
 
--- Finally, return the configuration to wezterm:
+-- Left Option sends Escape(Meta), Right Option sends Alt
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = true
+
+config.keys = {
+	{ key = "Backspace", mods = "CMD", action = act.SendKey({ key = "u", mods = "CTRL" }) },
+	{ key = "LeftArrow", mods = "CMD", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+	{ key = "RightArrow", mods = "CMD", action = act.SendKey({ key = "e", mods = "CTRL" }) },
+	{ key = "LeftArrow", mods = "OPT", action = act.SendKey({ key = "b", mods = "ALT" }) },
+	{ key = "RightArrow", mods = "OPT", action = act.SendKey({ key = "f", mods = "ALT" }) },
+
+	-- Weztemr specific window and tab management shortcuts
+	-- { key = "t", mods = "CMD", action = act.SpawnTab("CurrentPaneDomain") },
+	-- { key = "w", mods = "CMD", action = act.CloseCurrentTab({ confirm = true }) },
+	-- { key = "[", mods = "CMD", action = act.ActivateTabRelative(-1) },
+	-- { key = "]", mods = "CMD", action = act.ActivateTabRelative(1) },
+	--
+	-- { key = "d", mods = "CMD", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	-- { key = "d", mods = "CMD|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+}
+
 return config
